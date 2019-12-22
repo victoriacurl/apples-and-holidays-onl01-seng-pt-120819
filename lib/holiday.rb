6215db4d96ce28a37170aede49f5044366b45f72
Victoria Curl <victoriacurl9@gmail.com>
2019-12-22 18:07:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,7 +19,6 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # }
   holiday_hash[:summer][:fourth_of_july][1]
 end
-end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
